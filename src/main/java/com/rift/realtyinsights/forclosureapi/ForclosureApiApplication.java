fdd6b55bd03b6d5fc8b44663e2fa97c6adfe75dc
quangng2000 <quang.ngn2000@gmail.com>
2023-08-06 12:47:09 -0500

    initial commit

---
+++ src/main/java/com/rift/realtyinsights/forclosureapi/ForclosureApiApplication.java
@@ -0,0 +1,13 @@
+package com.rift.realtyinsights.forclosureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForclosureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForclosureApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rift/realtyinsights/forclosureapi/ForclosureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rift.realtyinsights.forclosureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForclosureApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
