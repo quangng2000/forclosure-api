d4d5f412898767eb47a68c78c16e16291137864c
quangng2000 <quang.ngn2000@gmail.com>
2023-08-21 12:52:57 -0500

    encrypted properties
    encrypted properties

--- src/main/java/com/rift/realtyinsights/forclosureapi/ForclosureApiApplication.java
+++ src/main/java/com/rift/realtyinsights/forclosureapi/ForclosureApiApplication.java
@@ -3,7 +3,10 @@ package com.rift.realtyinsights.forclosureapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ForclosureApiApplication {
 
 	public static void main(String[] args) {
