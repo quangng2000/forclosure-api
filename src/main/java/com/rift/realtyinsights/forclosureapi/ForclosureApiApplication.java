51d109a29d6be18d9a4afc147d68bddf75d20503
quangng2000 <quang.ngn2000@gmail.com>
2023-08-21 20:44:46 -0500

    Update ForclosureApiApplication.java

--- src/main/java/com/rift/realtyinsights/forclosureapi/ForclosureApiApplication.java
+++ src/main/java/com/rift/realtyinsights/forclosureapi/ForclosureApiApplication.java
@@ -2,11 +2,13 @@ package com.rift.realtyinsights.forclosureapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
 @EnableEncryptableProperties
+@CrossOrigin(origins = "*")
 public class ForclosureApiApplication {
 
 	public static void main(String[] args) {
